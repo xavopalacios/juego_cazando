d87e2ac6e06cd477544363f623c30d54369b5c0c
xavopalacios <xavopalacios@gmail.com>
2026-04-13 21:45:28 -0500

    The functions dibujarComida and iniciarJuego were created and referenced in the html the funtion iniciarJuego

--- cazando.js
+++ cazando.js
@@ -5,3 +5,14 @@ function dibujarGato(){
     ctx.fillStyle="gray";
     ctx.fillRect((canvas.width-50)/2,(canvas.height-60)/2,50,60);    
 }
+
+function dibujarComida(){
+    ctx.fillStyle="red";
+    //ctx.fillRect(Math.random()*(canvas.width-20),Math.random()*(canvas.height-20),20,20);    
+    ctx.fillRect(0,0,20,20);
+}
+
+function iniciarJuego(){
+    dibujarGato();
+    dibujarComida();
+}
