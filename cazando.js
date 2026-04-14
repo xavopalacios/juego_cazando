3472bd5e2556313d6e57ce5cc471bce54c9b49ee
xavopalacios <xavopalacios@gmail.com>
2026-04-13 22:15:31 -0500

    Zero variables and constants were created to establish the position and size values ​​of the cat and food, and they were replaced within each function, and in the iniciarJuego function the initial values ​​of each position variable were established

--- cazando.js
+++ cazando.js
@@ -1,18 +1,33 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let gatoX=0;
+let gatoY=0;
+let comidaX=0;
+let comidaY=0;
+
+const ALTO_GATO=60;
+const ANCHO_GATO=50;
+const ALTO_COMIDA=20;
+const ANCHO_COMIDA=20;
+
+function iniciarJuego(){
+    gatoX=(canvas.width-ANCHO_GATO)/2;
+    gatoY=(canvas.height-ALTO_GATO)/2;
+    comidaX=(ANCHO_COMIDA);
+    comidaY=(ALTO_COMIDA);
+
+    dibujarGato();
+    dibujarComida();
+}
 
 function dibujarGato(){
     ctx.fillStyle="gray";
-    ctx.fillRect((canvas.width-50)/2,(canvas.height-60)/2,50,60);    
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);    
 }
 
 function dibujarComida(){
     ctx.fillStyle="red";
     //ctx.fillRect(Math.random()*(canvas.width-20),Math.random()*(canvas.height-20),20,20);    
-    ctx.fillRect(0,0,20,20);
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
 }
 
-function iniciarJuego(){
-    dibujarGato();
-    dibujarComida();
-}
